e69c391d689029e5a1b3c12dcfa3059121d7e502
Michael Solinski <m.solinski@greyrook.com>
2019-06-03 16:25:17 +0200

    Init example project

---
+++ example/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import classNames from "classnames";
+
+const App = (): React.ReactElement => {
+  const [isSpinning, setIsSpinning] = useState<boolean>(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img  src={logo}
+          className={classNames({
+            "App-logo": true, "App-logo-animation": isSpinning
+          })}
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={():void => setIsSpinning(!isSpinning)}>
+          {isSpinning ? "Stop Spinner" : "Start Spinner"}
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default App;
