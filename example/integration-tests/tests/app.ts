a1f9501343bd71a0da08660524089170da55d7f0
Michael Solinski <m.solinski@greyrook.com>
2019-08-12 09:26:35 +0200

    Name test correctly and expect element to has css.

--- example/integration-tests/tests/app.ts
+++ example/integration-tests/tests/app.ts
@@ -5,8 +5,8 @@ module.exports = {
     browser.url("http://frontend:8080/");
     browser.expect.element(".App").to.be.present;
   },
-  'Test if logo can be toggled spinning': function (browser: any) {
+  'Test if logo is not spinning on load': function (browser: any) {
     browser.url("http://frontend:8080/");
-    browser.expect.element(".App-logo").has.not.css("class").which.not.contain(".App-logo-animation");
+    browser.expect.element(".App-logo").has.css("class").which.not.contain(".App-logo-animation");
   }
 };
