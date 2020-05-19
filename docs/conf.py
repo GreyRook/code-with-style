f539caf78904642923200cc1d2570d8db2f63105
Florian Ludwig <f.ludwig@greyrook.com>
2020-05-19 16:32:23 +0200

    fix company spelling

--- docs/conf.py
+++ docs/conf.py
@@ -4,23 +4,13 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 from sphinx_markdown_parser.parser import MarkdownParser
 
-project = 'guide'
-copyright = '2019, Greyrook GmbH'
-author = 'Greyrook GmbH'
+project = 'How to code with style'
+copyright = '2019-2020, Grey Rook GmbH'
+author = 'Grey Rook GmbH'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,6 +20,7 @@ author = 'Greyrook GmbH'
 # ones.
 extensions = [
 ]
+master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
