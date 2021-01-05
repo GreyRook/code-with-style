9633bb4312fbfaf6f1dceef7d0f7bb7965759846
Beshoy Abdelmalak <b.abdelmalak@greyrook.com>
2021-01-05 11:50:42 +0200

    check multiple file extensions

--- pre_commit_hooks/check_lfs_files.py
+++ pre_commit_hooks/check_lfs_files.py
@@ -9,7 +9,7 @@ def added_files():
     return set(added_files.stdout.splitlines())
 
 
-def check_lfs_files(file_type):
+def check_lfs_files(file_types):
     staged_files = added_files()
 
     cmd = ["git", "lfs", "status", "--json"]
@@ -17,16 +17,21 @@ def check_lfs_files(file_type):
     files_names = set(json.loads(lfs_files.stdout)["files"])
 
     for complete_name in staged_files:
-        file_name, file_extension = os.path.splitext(complete_name)
-        if file_type != file_extension:
+        _, file_extension = os.path.splitext(complete_name)
+
+        if file_extension not in file_types:
             continue
+
         if complete_name not in files_names:
+            print(
+                f"Extension {file_extension} should be tracked by lfs, however {complete_name} is not."
+            )
             return 1
     return 0
 
 
 def main():
-    return check_lfs_files("png")
+    return check_lfs_files([".png"])
 
 
 if __name__ == "__main__":
